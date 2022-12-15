03e0f4ab6037d6169117ec167bfab50e98fdae34
cesar jesus leon martin <cesilius98@gmail.com>
2022-12-15 10:39:18 +0100

    Feature: creado metodo run

--- src/Huerto.java
+++ src/Huerto.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 public class Huerto {
     private final int MAXIMO_VERDURA = 10;
-    private String verduras [] = new String [MAXIMO_VERDURA];
+    private ArrayList<String> verduras = new ArrayList<>(MAXIMO_VERDURA);
 
     public Huerto(){
 
     }
+    synchronized public void sembrar (String verdura , String productorName){
+        verduras.add(verdura);
+        System.out.println("una " + verdura+ "ha sido introducida por " + productorName);
+    }
+    synchronized public String recoger (String clienteName){
+       return verduras.remove(1);
+    }
+
+
 }
--- src/Productor.java
+++ src/Productor.java
@@ -1,13 +1,33 @@
-public class Productor {
+public class Productor extends Thread {
     private String name;
     private String verdura;
+    private Huerto huerto;
+    private int verdurasProducidas;
+    private int numeroSemilla;
 
-    public Productor (String name){
+    public Productor (String name ,int numeroSemilla, Huerto huerto){
         this.name = name;
+        this.numeroSemilla = numeroSemilla;
+        this.huerto = huerto;
 
     }
 
-    public void cultivar(){
+    public void generarSemilla(){
+        int numero = (int)(Math.random()*10+1);
+    }
+    @Override
+    public void run (){
+
+        try{
+
+            wait(5);
+            this.huerto.sembrar(this.verdura, this.name);
+            verdurasProducidas++;
+
+        }catch (InterruptedException e){
+            System.out.println("no produzco");
+        }
+
 
     }
 
