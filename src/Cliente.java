76d8c77bd5f94e80fc7d58d4e45913d5a9b6a440
cesar jesus leon martin <cesilius98@gmail.com>
2022-12-15 11:01:11 +0100

    Feature: generado constructor cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Cliente {
+
+    private String nombre;
+    private ArrayList<String> verdurasObtenidas;
+    private Tienda tienda;
+
+    public Cliente (String nombre, Tienda tienda){
+        this.nombre = nombre;
+        this.tienda = tienda;
+
+    }
+
+
+
 }
