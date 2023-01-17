0c2710a4951d8d2813ac9dd0ae4761b48887e090
cesar jesus leon martin <cesilius98@gmail.com>
2023-01-17 13:20:20 +0100

    Fix: correccion de salida

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        final int CONSUMICION = 10;
+        final int CONSUMICION = 5;
         Tienda miHuerto = new Tienda();
 
         Productor paco = new Productor("Paco",CONSUMICION,miHuerto);
-        Productor manuel = new Productor("manuel",CONSUMICION,miHuerto);
+
 
         Cliente jose = new Cliente("jose",CONSUMICION,miHuerto);
-        Cliente miguel = new Cliente("miguel",CONSUMICION,miHuerto);
+
 
         
         paco.start();
-        manuel.start();
+
 
         jose.start();
-        miguel.start();
+
 
 
     }
