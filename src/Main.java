4c13798bb4558dc037870996a8204b9a40aa3a7c
cesar jesus leon martin <cesilius98@gmail.com>
2023-01-17 13:37:35 +0100

    Fix: ultimos arreglos

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         final int CONSUMICION = 10;
-        Tienda miHuerto = new Tienda();
+        Tienda miTienda = new Tienda();
 
-        Productor paco = new Productor("Paco",CONSUMICION,miHuerto);
-        Productor manuel = new Productor("manuel",CONSUMICION,miHuerto);
+        Productor paco = new Productor("Paco",CONSUMICION,miTienda);
+        Productor manuel = new Productor("manuel",CONSUMICION,miTienda);
 
-        Cliente jose = new Cliente("jose",CONSUMICION,miHuerto);
-        Cliente miguel = new Cliente("miguel",CONSUMICION,miHuerto);
+        Cliente jose = new Cliente("jose",CONSUMICION,miTienda);
+        Cliente miguel = new Cliente("miguel",CONSUMICION,miTienda);
 
         
         paco.start();
